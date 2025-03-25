e8940d2f17eda04b4cc6de6b0f4a2066721ed977
sarvsaur <quic_sarvsaur@quicinc.com>
2025-03-25 17:26:54 +0530

    Update valid phone number for VoIP call
    Update valid phone number to remote device instead of empty phone number
    for VoIP call
    
    Bug: 406175098
    Change-Id: I23e1672086b85e60ed25954bc6d87dd33af573d2
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -2137,10 +2137,11 @@ class HeadsetStateMachine extends StateMachine {
         if (mHeadsetService.isVirtualCallStarted()) {
             // In virtual call, send our phone number instead of remote phone number
             String phoneNumber = mSystemInterface.getSubscriberNumber();
-            if (phoneNumber == null) {
-                phoneNumber = "";
+            if (phoneNumber == null || phoneNumber == "") {
+                phoneNumber = "10000000";
             }
             int type = PhoneNumberUtils.toaFromString(phoneNumber);
+            Log.i(TAG, "processAtClcc: voip phoneNumber " + phoneNumber +" voip type " + type);
             mNativeInterface.clccResponse(device, 1, 0, 0, 0, false, phoneNumber, type);
             mNativeInterface.clccResponse(device, 0, 0, 0, 0, false, "", 0);
         } else if (hasMessages(CLCC_RSP_AFTER_VOIP_CALL_END)) {
